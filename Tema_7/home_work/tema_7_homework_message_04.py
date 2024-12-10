b31e292fb74a2b566a441267a8a460176c7e25b8
unknown <jessc@yandex.ru>
2024-12-10 15:33:02 +0300

    added logging in homework_message_04

--- Tema_7/home_work/tema_7_homework_message_04.py
+++ Tema_7/home_work/tema_7_homework_message_04.py
@@ -1,14 +1,25 @@
 import asyncio
+import logging
 import time
 
 from eth_account.messages import encode_defunct, encode_typed_data
 from web3 import AsyncWeb3
 
+# Настройка логгера
+logging.basicConfig(filename='myapp.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+PRIVATE_KEY = '93dce5fbe9057709f6c41fe494ee37baad2a8dcdaf78cb9711961c48cbc36cd1'
+
 class Client:
-    def __init__(self):
-        self.private_key = ''
+    def __init__(self, private_key):
+        if not private_key:
+            raise ValueError("Приватный ключ не может быть пустым.")
+        self.private_key = private_key
         self.w3 = AsyncWeb3()
         self.address = self.w3.to_checksum_address(self.w3.eth.account.from_key(self.private_key).address)
+        if not self.private_key:
+            raise ValueError("Приватный ключ не указан. Проверьте настройки клиента.")
 
     def custom_message(self, data_to_sign, eip_712_data: bool = False):
 
@@ -89,19 +100,32 @@ async def prepare_permit_data():
     return permit_data
 
 async def verification_signature(client, message, signature):
-    recovered_address = client.w3.eth.account.recover_message(message, signature=signature)
-    print(f"Восстановленный адрес: {recovered_address}")
+    try:
+        recovered_address = client.w3.eth.account.recover_message(message, signature=signature)
+        logger.info(f"Восстановленный адрес: {recovered_address}")
+
+        if recovered_address.lower() == client.address.lower():
+            logger.info("Подпись успешно проверена!")
+        else:
+            logger.error("Ошибка: восстановленный адрес не совпадает с ожидаемым.")
+    except Exception as e:
+        logger.error(f"Ошибка при проверке подписи: {e}")
+
 
 async def main():
-    client = Client()
+    client = Client(PRIVATE_KEY)
 
     permit_data = await prepare_permit_data()
-    signable_message = client.custom_message(data_to_sign=permit_data, eip_712_data=True)
+    try:
+        signable_message = client.custom_message(data_to_sign=permit_data, eip_712_data=True)
+    except Exception as e:
+        print(f"Ошибка при создании подписываемого сообщения: {e}")
+        return
 
     signed_message = client.w3.eth.account.sign_message(signable_message=signable_message, private_key=client.private_key)
-    print("Сообщение успешно подписано!")
+    logger.info("Сообщение успешно подписано!")
     signature = client.w3.to_hex(signed_message.signature)
-    print(f"Сигнатура: {signature}")
+    logger.info(f"Signature: {signature}")
 
     await verification_signature(client, signable_message, signature)
 
